f3878729c16801944e2075b44a03bbc411e82734
Pritam Sarkar <104849674+prikarsartam@users.noreply.github.com>
2023-01-20 03:46:48 +0530

    Add files via upload

--- scrape_and_summarise_websites.py
+++ scrape_and_summarise_websites.py
@@ -35,21 +35,31 @@ response = openai.Completion.create(
 # Print the summary
 print(response["choices"][0]["text"])
 
-# Ask a question based on the parsed HTML
-question = input("\nWhat do you want to know? ")
+a=1
 
-# Use the OpenAI API to answer the question
-response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt=f"Based on the information from the website {url}, {question}",
-  temperature=0.5,
-  max_tokens=100,
-  top_p=1,
-  frequency_penalty=1,
-  presence_penalty=1,
-  stop=f"{text}"
-)
+while a>0:
+  # Ask a question based on the parsed HTML
+  question = input("\nWhat do you want to know? ")
+  if question != '0':
+    a++
+      # Use the OpenAI API to answer the question
+    response = openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=f"Based on the information from the website {url}, {question}",
+      temperature=0.5,
+      max_tokens=100,
+      top_p=1,
+      frequency_penalty=1,
+      presence_penalty=1,
+      stop=f"{text}"
+    )
 
 
-# Print the answer
-print(response["choices"][0]["text"])
+    # Print the answer
+    print(response["choices"][0]["text"])
+
+  else:
+    a=0
+
+
+
